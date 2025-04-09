9e7e708e752e0e91cc8212c5853f59a544e13b97
MohamadKASSEM <mohamadkassem101@gmail.com>
2025-04-09 21:17:15 +0300

    "first commit"

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-start items-center">
+      <Text>Bay bay Native.</Text>
+    </View>
+  );
+}
