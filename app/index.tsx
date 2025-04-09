27d4384e8eee5e62a8e0771cc12ac8939dfc4048
MohamadKASSEM <mohamadkassem101@gmail.com>
2025-04-09 21:41:14 +0300

    new branch

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router"
 
 export default function Index() {
   return (
     <View className="flex-1 justify-start items-center">
-      <Text>Bay bay Native.</Text>
+      <Text className="text-blue-700">Bay bay Native.</Text>
+      <Link href="/OnBoarding" className="text-blue-500">go to onBoarding</Link>
     </View>
   );
 }
